4b43d2cdc2131e82cdc1e7e9971c93fdfea7935a
Daniil Aksonov <aksunov1@gmail.com>
2025-02-10 17:59:56 +0100

    feat: Add adapability ✨

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,17 @@
+const header = document.querySelector(".header");
+const headerOffsetHeight = header.offsetHeight;
+
+window.addEventListener("scroll", function(){
+  const className = "header--fixed";
+  if (this.scrollY > headerOffsetHeight)
+    header.classList.add(className);
+  else
+    header.classList.remove(className);
+});
+
 const tabsWrap = document.querySelector(".tabs");
 
 tabsWrap.addEventListener("click", function(e){
-  if (!(
-    e.target.classList.contains("tabs__tab") ||
-    e.target.parentNode.classList.contains("tabs__tab") ||
-    e.target.parentNode.classList.contains("tabs__svg")
-  ))
-    return;
-
-  let activeClass = "tabs__tab--active";
-  tabsWrap.querySelector("." + activeClass).classList.remove(activeClass);
-
   let targetTab;
   if (e.target.tagName === "use")
     targetTab = e.target.parentNode.parentNode;
@@ -18,7 +19,11 @@ tabsWrap.addEventListener("click", function(e){
     targetTab = e.target.parentNode;
   else if (e.target.tagName === "BUTTON")
     targetTab = e.target;
+  else
+    return;
 
+  let activeClass = "tabs__tab--active";
+  tabsWrap.querySelector("." + activeClass).classList.remove(activeClass);
   targetTab.classList.add(activeClass);
 
   activeClass = "products__list--active";
@@ -31,20 +36,69 @@ tabsWrap.addEventListener("click", function(e){
     listTarget.classList.add(activeClass);
 });
 
-const swiper = new Swiper('.reviews-slider', {
-  loop: true,
-
+const reviewsSwiper = new Swiper(".reviews-slider", {
   navigation: {
-    nextEl: '.reviews-slider__btn--next',
-    prevEl: '.reviews-slider__btn--prev',
+    nextEl: ".reviews-slider__btn--next",
+    prevEl: ".reviews-slider__btn--prev",
   },
   a11y: {
-    prevSlideMessage: 'Попередній слайд',
-    nextSlideMessage: 'Наступний слайд'
+    prevSlideMessage: "Попередній слайд",
+    nextSlideMessage: "Наступний слайд"
   },
   pagination: {
-    el: '.reviews-slider__pagination',
-    type: 'bullets',
+    el: ".reviews-slider__pagination",
+    type: "bullets",
     clickable: true
+  },
+  breakpoints: {
+    320: {
+      pagination: {
+        el: null,
+        enabled: false
+      }
+    },
+
+    560: {
+      pagination: {
+        el: ".reviews-slider__pagination",
+        type: "bullets",
+        clickable: true
+      }
+    }
   }
 });
+
+var restaurantsSwiper;
+loadRestaurantsSwiper();
+window.addEventListener("resize", debounce(loadRestaurantsSwiper));
+
+function loadRestaurantsSwiper() {
+  if (window.innerWidth <= 768) {
+    if (document.querySelector(".restaurants__swiper-wrap.swiper-initialized")) {
+      console.log("init");
+      return;
+    }
+
+    console.log("initiate");
+
+    restaurantsSwiper = new Swiper(".restaurants__swiper-wrap", {
+      slidesPerView: 1,
+
+      pagination: {
+        el: ".restaurants__swiper-pagination",
+        type: "bullets",
+        clickable: true
+      }
+    });
+  } else if (document.querySelector(".restaurants__swiper-wrap.swiper-initialized")) {
+    restaurantsSwiper.destroy();
+  }
+}
+
+function debounce(func){
+  var timer;
+  return function(event){
+    if(timer) clearTimeout(timer);
+    timer = setTimeout(func,100,event);
+  };
+}
