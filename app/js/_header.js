a6fcdd89afe90b8d0cda94b19a97eb5c5aa06eaa
Daniil Aksonov <aksunov1@gmail.com>
2025-06-12 14:49:37 +0200

    fix: replace this with window in setFixedHeader 🐛

--- app/js/_header.js
+++ app/js/_header.js
@@ -1,6 +1,6 @@
 function setFixedHeader() {
   const className = 'header__wrapper--fixed';
-  if (this.scrollY > 0) {
+  if (window.scrollY > 0) {
     header.classList.add(className);
   } else {
     header.classList.remove(className);
