37cbedd50f9558202dd1731e14b97bcb66d9f5cf
Daniil Aksonov <aksunov1@gmail.com>
2025-08-01 12:29:17 +0200

    feat: add radio buttons rating in form ✨

---
+++ app/js/_rating-arrows.js
@@ -0,0 +1,23 @@
+const fieldsets = getAllEls('.rating');
+
+for (ratingFieldset of fieldsets) {
+  const inputs = [...ratingFieldset.querySelectorAll('.rating__input')];
+
+  if (inputs.length) {
+    ratingFieldset.addEventListener('keydown', e => {
+      if (e.key !== 'ArrowLeft' && e.key !== 'ArrowRight') return;
+      e.preventDefault();
+
+      const currentIndex = inputs.findIndex(input => input.checked);
+      if (currentIndex === -1) return;
+
+      const delta = e.key === 'ArrowLeft' ? -1 : 1;
+      const newIndex = currentIndex - delta;
+
+      if (inputs[newIndex]) {
+        inputs[newIndex].checked = true;
+        inputs[newIndex].focus();
+      }
+    });
+  }
+}
