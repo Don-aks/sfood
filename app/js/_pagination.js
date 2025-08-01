7ff117288704cdaa5ba0eb6e91d93645a54d27ca
Daniil Aksonov <aksunov1@gmail.com>
2025-08-01 11:36:52 +0200

    refactor: set normal focus on pagination 📦

--- app/js/_pagination.js
+++ app/js/_pagination.js
@@ -1,26 +1,18 @@
 const pagination = getEl('.pagination');
-let pages = pagination?.querySelectorAll('a.pagination__link');
+let pageLinks = pagination?.querySelectorAll('a.pagination__link');
 
 function initPagination() {
   if (!pagination) return;
 
-  function setFocus(index) {
-    pages.forEach((page, i) => {
-      page.setAttribute('tabindex', i === index ? '0' : '-1');
-    });
-    pages[index].focus();
-  }
-
   let currentIndex = 0;
-
   pagination.addEventListener('keydown', e => {
     if (e.key === 'ArrowRight') {
-      currentIndex = (currentIndex + 1) % pages.length;
-      setFocus(currentIndex);
+      currentIndex = (currentIndex + 1) % pageLinks.length;
+      pageLinks[currentIndex].focus();
       e.preventDefault();
     } else if (e.key === 'ArrowLeft') {
-      currentIndex = (currentIndex - 1 + pages.length) % pages.length;
-      setFocus(currentIndex);
+      currentIndex = (currentIndex - 1 + pageLinks.length) % pageLinks.length;
+      pageLinks[currentIndex].focus();
       e.preventDefault();
     }
   });
