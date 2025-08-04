03bda113569947e7f81733b9ede4eb60f868a24e
Daniil Aksonov <aksunov1@gmail.com>
2025-08-04 11:58:48 +0200

    style: remove event var from dualRange listener 💎

--- app/js/main.js
+++ app/js/main.js
@@ -42,7 +42,7 @@ new Swiper('.modal-swiper__carousel', config.modalSwiper);
 const inputMin = getEl('.js-input-min');
 const inputMax = getEl('.js-input-max');
 if (getEl(config.selector)) {
-  dualRange(config.range).on('change', (e, { start, end }) => {
+  dualRange(config.range).on('change', (_, { start, end }) => {
     inputMin.value = start;
     inputMax.value = end;
   });
